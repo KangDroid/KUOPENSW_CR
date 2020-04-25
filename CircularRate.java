2c4ab7e317c2e922bf268a7fbb46766164a9172e
KangDroid <hyunwoo286@gmail.com>
2020-04-25 16:46:38 +0900

    Write full sc

--- CircularRate.java
+++ CircularRate.java
@@ -1,4 +1,10 @@
 class CircularRate {
     public static void main(String[] args) {
+        int max = 1000000;
+        double sum = 0;
+        for (int i = 1; i <= max; i++) {
+            sum += (Math.pow(-1, i+1)) / ((2 * i) - 1);
+        }
+        System.out.println("Val: " + (4 * sum));
     }
 }
